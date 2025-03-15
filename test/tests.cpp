281392ae35e86b2e13c6bbe06ba8ed0355ba264c
MikeMuradov <133360712+MikeMuradov@users.noreply.github.com>
2025-03-15 16:24:05 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,14 +4,55 @@
 #include <cstdint>
 #include "alg.h"
 
-
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(CheckPrimeTest, HandlesZeroAndOne) {
+  EXPECT_FALSE(checkPrime(0));
+  EXPECT_FALSE(checkPrime(1));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(CheckPrimeTest, HandlesSmallPrimes) {
+  EXPECT_TRUE(checkPrime(2));
+  EXPECT_TRUE(checkPrime(3));
+  EXPECT_TRUE(checkPrime(5));
+}
+
+TEST(CheckPrimeTest, HandlesNonPrimes) {
+  EXPECT_FALSE(checkPrime(4));
+  EXPECT_FALSE(checkPrime(9));
+  EXPECT_FALSE(checkPrime(15));
+}
+
+TEST(NPrimeTest, FindsFirstPrime) {
+  EXPECT_EQ(nPrime(1), 2);
+}
+
+TEST(NPrimeTest, FindsTenthPrime) {
+  EXPECT_EQ(nPrime(10), 29);
+}
+
+TEST(NPrimeTest, FindsHundredthPrime) {
+  EXPECT_EQ(nPrime(100), 541);
+}
+
+TEST(NextPrimeTest, FindsNextAfterTwo) {
+  EXPECT_EQ(nextPrime(2), 3);
+}
+
+TEST(NextPrimeTest, FindsNextAfterTen) {
+  EXPECT_EQ(nextPrime(10), 11);
+}
+
+TEST(NextPrimeTest, FindsNextAfterLargeNumber) {
+  EXPECT_EQ(nextPrime(1000), 1009);
+}
+
+TEST(SumPrimeTest, SumPrimesBelowTen) {
+  EXPECT_EQ(sumPrime(10), 17);
+}
+
+TEST(SumPrimeTest, SumPrimesBelowTwenty) {
+  EXPECT_EQ(sumPrime(20), 77);
+}
+
+TEST(SumPrimeTest, SumPrimesBelowHundred) {
+  EXPECT_EQ(sumPrime(100), 1060);
 }
